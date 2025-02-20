7a50c23700cc3a7944a2f63c4c44518f790b8c63
Jan Fischer <fischerjan870@gmail.com>
2025-02-20 12:58:19 +0100

    Change

--- email/index.tsx
+++ email/index.tsx
@@ -8,16 +8,10 @@ require("dotenv").config();
 const resend = new Resend(process.env.RESEND_API_KEY as string);
 
 export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
-  console.log("pička posrana", order);
-
-  try {
-    await resend.emails.send({
-      from: `${APP_NAME} <${SENDER_EMAIL}>`,
-      to: order.user.email,
-      subject: `Order Confirmation ${order.id}`,
-      react: <PurchaseReceiptEmail order={order} />,
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  await resend.emails.send({
+    from: `${APP_NAME} <${SENDER_EMAIL}>`,
+    to: order.user.email,
+    subject: `Order Confirmation ${order.id}`,
+    react: <PurchaseReceiptEmail order={order} />,
+  });
 };
